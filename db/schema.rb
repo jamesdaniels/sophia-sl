024759cdbdb8028888e1d7cefab4794c6c3daeef
James Daniels <james@james-danielss-macbook.local>
2009-05-18 02:46:01 -0400

    Accounts

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,29 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090518045743) do
+ActiveRecord::Schema.define(:version => 20090518061524) do
 
   create_table "slides", :force => true do |t|
     t.string   "original_file_name"
     t.text     "meta_data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                              :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
